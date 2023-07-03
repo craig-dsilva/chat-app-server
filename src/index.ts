d3a81913481c1e5c360a38ac1a16aa391fbd1499
craig-dsilva <craig.s.dsilva@gmail.com>
2023-07-03 12:10:14 +0100

    Setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import express from 'express';
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io';
 
 const app = express()
+app.use(cors())
+const PORT = 5000
 
-app.listen(5000)
+const server = http.createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id)
+    })
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
